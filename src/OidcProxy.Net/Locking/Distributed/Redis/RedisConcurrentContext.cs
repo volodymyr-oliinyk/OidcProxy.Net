d4fc2d590a08de04d5ab429cc549e6fe40b369de
voliinyk <volodymyr.oliinyk@zorallabs.com>
2025-07-17 17:07:24 +0300

    fixing token  renewal - 1.0.6

--- src/OidcProxy.Net/Locking/Distributed/Redis/RedisConcurrentContext.cs
+++ src/OidcProxy.Net/Locking/Distributed/Redis/RedisConcurrentContext.cs
@@ -1,3 +1,5 @@
+using System.Collections.Concurrent;
+using System.Threading;
 using Microsoft.AspNetCore.Http;
 using RedLockNet;
 
@@ -5,11 +7,13 @@ namespace OidcProxy.Net.Locking.Distributed.Redis;
 
 public class RedisConcurrentContext(IDistributedLockFactory redisLockFactory) : IConcurrentContext
 {
+    private readonly ConcurrentDictionary<string, CancellationTokenSource> _cancellationToken = new();
+    
     public async Task ExecuteOncePerSession(ISession session, string identifier, Func<bool> actionRequired, Func<Task> @delegate)
     {
         var expiryTime = TimeSpan.FromSeconds(15);
-        var waitTime = TimeSpan.FromMilliseconds(100);
-        var retryTime = TimeSpan.FromSeconds(5);
+        var waitTime = TimeSpan.FromSeconds(10);
+        var retryTime = TimeSpan.FromSeconds(1);
         
         if (!actionRequired())
         { 
@@ -18,20 +22,43 @@ public class RedisConcurrentContext(IDistributedLockFactory redisLockFactory) : 
         
         var cacheKey = $"{typeof(RedisConcurrentContext).FullName}+{session.Id}+{identifier}";
 
-        await using var resourceLock = await redisLockFactory.CreateLockAsync(cacheKey, expiryTime, waitTime, retryTime);
-        
-        var isActionRequired = actionRequired();
-        if (!resourceLock.IsAcquired && isActionRequired)
-        {
-            throw new ApplicationException($"Unable to renew the expired access_token. Unable to acquire a lock. " +
-                                           $"Try again. Error: {resourceLock.InstanceSummary.ToString()}");
-        }
-        
-        if (!isActionRequired)
-        { 
-            return;
-        }
+        var cancellationTokenSource = _cancellationToken.GetOrAdd(cacheKey, _ => new CancellationTokenSource());
 
-        await @delegate();
+        IRedLock? resourceLock = null;
+
+        try
+        {
+            resourceLock = await redisLockFactory.CreateLockAsync(cacheKey, expiryTime, waitTime, retryTime, cancellationTokenSource.Token);
+
+            var isActionRequired = actionRequired();
+            if (!resourceLock.IsAcquired && isActionRequired)
+            {
+                throw new ApplicationException($"Unable to renew the expired access_token. Unable to acquire a lock. " +
+                                               $"Try again. Error: {resourceLock.InstanceSummary.ToString()}");
+            }
+
+            if (!isActionRequired)
+            {
+                return;
+            }
+
+            await @delegate();
+        }
+        catch (OperationCanceledException)
+        {
+            await ExecuteOncePerSession(session, identifier, actionRequired, @delegate);
+        }
+        finally
+        {
+            if (!cancellationTokenSource.IsCancellationRequested)
+            {
+                await cancellationTokenSource.CancelAsync();
+            }
+
+            _cancellationToken.TryRemove(cacheKey, out _);
+            cancellationTokenSource.Dispose();
+            if (resourceLock != null)
+                await resourceLock.DisposeAsync();
+        }
     }
 }
--- src/OidcProxy.Net/Middleware/TokenRenewalMiddleware.cs
+++ src/OidcProxy.Net/Middleware/TokenRenewalMiddleware.cs
@@ -5,7 +5,7 @@ using OidcProxy.Net.OpenIdConnect;
 
 namespace OidcProxy.Net.Middleware;
 
-internal class TokenRenewalMiddleware(TokenFactory tokenFactory, ILogger logger, ISkipAuthRoutes skipAuthRoutes) : IYarpMiddleware
+internal class TokenRenewalMiddleware(TokenFactory tokenFactory, ILogger logger, ISkipAuthRoutes skipAuthRoutes, IRedirectUriFactory redirectUriFactory, EndpointName endpointName) : IYarpMiddleware
 {
     public async Task Apply(HttpContext context, Func<HttpContext, Task> next)
     {
@@ -19,9 +19,11 @@ internal class TokenRenewalMiddleware(TokenFactory tokenFactory, ILogger logger,
             catch (TokenRenewalFailedException e)
             {
                 await logger.ErrorAsync(e);
-
-                context.Response.StatusCode = StatusCodes.Status401Unauthorized;
-                await context.Response.WriteAsync(@"{ ""reason"": ""token_renewal_failed"" }");
+                var url = redirectUriFactory.DetermineHostName(context) + endpointName + "/sign_out";
+                await logger.WarnAsync($"Token renewal failed. Redirecting to {url}.");
+                context.Response.Redirect(url);
+                //context.Response.StatusCode = StatusCodes.Status401Unauthorized;
+                //await context.Response.WriteAsync(@"{ ""reason"": ""token_renewal_failed"" }");
                 return; // And stop the pipeline here. The request will not be forwarded down-stream.
             }
         }
